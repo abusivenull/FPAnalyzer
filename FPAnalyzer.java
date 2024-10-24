75c476862973bfba40170185c9dad7f825b7d7d0
Manu <manuel.jimenez.ochoa@alumnos.upm.es>
2024-10-25 00:03:47 +0200

    Node and TypeOfFunction classes should be done. Further testing is needed.

--- FPAnalyzer.java
+++ FPAnalyzer.java
@@ -9,6 +9,7 @@ public class FPAnalyzer{
 	public static List<Node> allNodes;		// Esto se puede mantener como List. Que sea el usuario el que se encargue de asegurarse de que no repite nombres.
 	public static List<Edge> allEdges;
 	public static List<TypeOfFunction> allFunctions;
+	public static int total = 0;
 	
 	public static class Node{
 		String name;
@@ -27,33 +28,49 @@ public class FPAnalyzer{
 		// Changes node's weight when adjacent node's state is changed
 		public void addWeight(int value){
 			this.weight = this.weight + value;
+			if (isSelected){
+				updateTotal(value);
+			}
 		}
 		
+		/*
 		public void addFunction(TypeOfFunction function){
 			this.functions.add(function);
 		}
+		*/
 		
 		// Connects this node to an edge
-		public void addEdge(Edge edge){
+		public void addEdge(Edge edge, TypeOfFunction function){
 			this.edges.add(edge);
+			this.functions.add(function);
 		}
 		
 		// Changes node's state
 		public void select(){
 			isSelected = !isSelected;
-			for (int i = 0; i < functions.size(); ++i){
-				functions.get(i).updateNodes(this);
+			if (isSelected){
+				updateTotal(weight);
 			}
+			else{
+				updateTotal(-weight);
+			}
+			for (int i = 0; i < functions.size(); ++i){
+				functions.get(i).nodeWasSelected(this);
+			}
+			/*	Edges still not implemented
 			for(int i = 0; i < this.edges.size(); ++i){
 				this.edges.get(i).updateNodes(this);
 			}
+			*/
 		}
 		
 		// Shows all edges
 		public void showAllEdges(){
 			System.out.println("My shared function types are:");
 			for(int i = 0; i < edges.size(); ++i){
-				System.out.println(edges.get(i).function.name);
+				for (int j = 0; j < functions.size(); ++j){
+					System.out.println(edges.get(i).functions.get(j).name);
+				}
 			}
 		}
 	}
@@ -63,48 +80,53 @@ public class FPAnalyzer{
 		int weight;
 		List<Node> nodes;
 		List<Edge> edges;
-		int isImplemented = 0;
+		Node implementingNode;	// It is the node that assumes this function's weight. It must be a selected node. If no node is implementing this function, this attribute will be null.
+		int isImplemented = 0;	// Number of nodes selected
 		
 		// Creates a type of function without associated nodes.
-		public TypeOfFunction(String name, int weight){
+		public TypeOfFunction(String name, int weight) throws Exception{
+			System.out.println("Created without nodes");
 			if (weight < 0){
 				throw new Exception();
 			}
 			this.name = name;
 			this.weight = weight;
 			this.nodes = new ArrayList<Node>(10);
-			this.edges = new ArrayList(10);
+			this.edges = new ArrayList<Edge>(10);
+			System.out.println("Reached the end");
+			debugTypeOfFunction();
 		}
 		
-		// Creates a type of function without associated nodes. Used when creating types of function for an specific node.
-		public TypeOfFunction(String name, int weight, Node node1){
+		// Creates a type of function with one node. Used when creating types of function for an specific node.
+		public TypeOfFunction(String name, int weight, Node node) throws Exception{
+			System.out.println("Created with one node");
 			if (weight < 0){
 				throw new Exception();
 			}
 			this.name = name;
 			this.weight = weight;
 			this.nodes = new ArrayList<Node>(10);
-			this.nodes.add(node1);
-			this.edges = new ArrayList(10);
-			if (node1.isSelected){
-				this.isImplemented += 1;
-			}
+			this.edges = new ArrayList<Edge>(10);
+			addNode(node);
+			System.out.println("Reached the end");
+			debugTypeOfFunction();
 		}
 		
 		// Creates a type of function with two nodes. Used when two nodes are connected mannualy.
-		public TypeOfFunction(String name, int weight, Node node1, Node node2, Edge edge){
+		public TypeOfFunction(String name, int weight, Node node1, Node node2, Edge edge) throws Exception{
+			System.out.println("Created with two nodes");
 			if (weight < 0){
 				throw new Exception();
 			}
 			this.name = name;
 			this.weight = weight;
 			this.nodes = new ArrayList<Node>(10);
-			this.nodes.add(node1);
-			this.nodes.add(node2);
-			this.edges = new ArrayList(10);
+			this.edges = new ArrayList<Edge>(10);
+			addNode(node1);
+			addNode(node2);
 			this.edges.add(edge);
-			updateNodes(node1);
-			updateNodes(node2);
+			System.out.println("Reached the end");
+			debugTypeOfFunction();
 		}
 		
 		// Creates a type of function with several nodes. Used when assigning a group of nodes to a non-existing type of function.
@@ -113,51 +135,154 @@ public class FPAnalyzer{
 			this.name = name;
 			this.weight = weight;
 			this.nodes = new ArrayList<Node>(10);
-			this.nodes = nodes;
 			for (int i = 0; i < nodes.size(); ++i){
-				updateNodes(nodes.get(i));
+				addNode(nodes.get(i));
 			}
 		}
 		*/
 		
-		// Adds a node that uses this type of function. Used when adding a node to an existing type of function.
+		// Adds a node that uses this type of function.
 		public boolean addNode(Node node){
-			if (!nodes.contains(node)){
-				nodes.add(node);
-				if (this.isImplemented > 0){
-					node.addWeight(-this.weight);
-				}
-				if (node.isSelected){
-					isImplemented += 1;
-					updateNodes(node);
-				}
-				return true;
-			}
-			else{
-				return false;
-			}
-		}
-		
-		// Updates, if necessary, all nodes that use this type of function. This is called inmediatly after a node has changed its state or when a node has added this type of function.
-		public int updateNodes(Node node){
-			if (nodes.contains(node)){
+			boolean wasAdded = nodes.add(node);
+			if (implementingNode != null){
+				node.addWeight(-this.weight);
 				if (node.isSelected){
 					this.isImplemented += 1;
 				}
-				else{
-					this.isImplemented -= 1;
+			}
+			else{
+				if (node.isSelected){
+					this.isImplemented += 1;
+					implementingNode = node;
+					updateNodes();
 				}
-				if (node.isSelected && isImplemented == 1){		// This type of function has just been implemented -> Update nodes
-					for(int i = 0; i < nodes.size(); ++i){
-						nodes.get(i).addWeight(-this.weight);
+			}
+			
+			/*Son equivalentes
+			if (isSelected){
+				this.isImplemented += 1;
+				if (implementingNode != null){
+					node.addWeight(-this.weight);
+				}
+				else{
+					implementingNode = node;
+					updateNodes();				//Puede ser que todos los nodos de la red estén desactivados
+				}
+			}
+			else if(!isSelected && implementingNode != null){
+				node.addWeight(-this.weight);
+			}
+			*/
+			return wasAdded;
+		}
+		
+		// Removes a node that used this type of function.
+		public boolean removeNode(Node node){
+			boolean wasRemoved = nodes.remove(node);
+			if (wasRemoved){
+				if (node.isSelected){
+					this.isImplemented -= 1;
+					if (implementingNode == node){
+						chooseNewImplementingNode();
+						if (implementingNode != null){
+							implementingNode.addWeight(this.weight);
+						}
+						else{
+							updateNodes();
+						}
+					}
+					else if(implementingNode != node && implementingNode != null){
+						node.addWeight(this.weight);
 					}
 				}
-				else if (!node.isSelected && isImplemented == 0){	// This type of function has just stopped being implemented -> Update nodes
-					for(int i = 0; i < nodes.size(); ++i){
-						nodes.get(i).addWeight(this.weight);
+				else{
+					if (implementingNode != node && implementingNode != null){
+						node.addWeight(this.weight);
 					}
 				}
 			}
+			return wasRemoved;
+		}
+		
+		// Chooses a new node to assume this function's weight.
+		private void chooseNewImplementingNode(){
+			int i = 0;
+			while (i < nodes.size() && !nodes.get(i).isSelected){
+				i++;
+			}
+			if (i < nodes.size()){
+				implementingNode = nodes.get(i);
+				nodes.get(i).addWeight(this.weight);
+			}
+			else{
+				implementingNode = null;
+			}
+		}
+		
+		// Updates the number of nodes that are implementing this function based on if node is selected or not, and assigns an implementing node if there is none.
+		public void nodeWasSelected(Node node){
+			if(nodes.contains(node)){
+				if (node.isSelected){
+					this.isImplemented += 1;
+					if (implementingNode == null){
+						implementingNode = node;
+						updateNodes();
+					}
+				}
+				else{
+					this.isImplemented -= 1;
+					if (implementingNode == node){
+						chooseNewImplementingNode();
+						node.addWeight(-this.weight);
+						if (implementingNode == null){
+							updateNodes();
+						}
+					}
+				}
+				debugTypeOfFunction();
+			}
+		}
+		
+		// Updates, if necessary, all nodes that use this type of function, except for the implementing node. This is called inmediatly after a node implementing this function has changed its state or when a node has added or removed this type of function.
+		public int updateNodes(){
+			int numberOfNodesUpdated = 0;
+			if (isImplemented == 1){		// This type of function has just been implemented -> Update nodes
+				for(int i = 0; i < nodes.size(); ++i){
+					if (nodes.get(i) != implementingNode){
+						nodes.get(i).addWeight(-this.weight);
+						numberOfNodesUpdated += 1;
+					}
+				}
+			}
+			else if (isImplemented == 0){	// This type of function has just stopped being implemented -> Update nodes
+				for(int i = 0; i < nodes.size(); ++i){
+					if (nodes.get(i) != implementingNode){
+						nodes.get(i).addWeight(this.weight);
+						numberOfNodesUpdated += 1;
+					}
+				}
+			}
+			return numberOfNodesUpdated;
+		}
+		
+		private void debugTypeOfFunction(){
+			System.out.println("\tName = " + this.name);
+			System.out.println("\tWeight = " + this.weight);
+			for(int i = 0; i < nodes.size(); ++i){
+				System.out.println("\t" + nodes.get(i).name);
+			}
+			/*
+			for(int i = 0; i < edges.size(); ++i){
+				System.out.println("\t[" + edges.get(i).node1.name + ", " + edges.get(i).node2.name + "]");
+			}
+			*/
+			if(implementingNode == null){
+				System.out.println("\timplementingNode = " + this.implementingNode);
+			}
+			else{
+				System.out.println("\timplementingNode = " + this.implementingNode.name);
+			}
+			System.out.println("\tisImplemented = " + this.isImplemented);
 		}
 	}
 	
@@ -167,24 +292,30 @@ public class FPAnalyzer{
 		int weight = 0;
 		List<TypeOfFunction> functions;
 		
-		public Edge(String name, Node node1, Node node2, int weight){		//Tener cuidado con si se reutiliza el nombre de un tipo de función existente con un peso distinto
+		public Edge(String name, Node node1, Node node2, int weight) throws Exception{		//Tener cuidado con si se reutiliza el nombre de un tipo de función existente con un peso distinto
+			this.node1 = node1;
+			this.node2 = node2;
 			this.functions = new ArrayList<TypeOfFunction>(10);
 			int i = 0;
 			while(i < allFunctions.size() && !allFunctions.get(i).name.equals(name)){
 				++i;
 			}
 			if(i < allFunctions.size()){
-				this.functions.add(allFunctions.get(i));
-				allFunctions.get(i).addNode(node1);		// No importa si el nodo ya estaba
-				allFunctions.get(i).addNode(node2);		// No importa si el nodo ya estaba
-				this.weight = this.weight + allFunctions.get(i).updateNodes();
+				TypeOfFunction function = allFunctions.get(i);
+				this.functions.add(function);
+				//allFunctions.get(i).addNode(node1);		// No importa si el nodo ya estaba
+				//allFunctions.get(i).addNode(node2);		// No importa si el nodo ya estaba
+				this.weight = this.weight;
+				node1.addEdge(this, function);
+				node2.addEdge(this, function);
 			}
 			else{
-				this.functions.add(new TypeOfFunction(name, weight, node1, node2));
+				TypeOfFunction function = new TypeOfFunction(name, weight, node1, node2, this);
+				this.functions.add(function);
 				this.weight = this.weight + weight;
+				node1.addEdge(this, function);
+				node2.addEdge(this, function);
 			}
-			this.node1 = node1;
-			this.node2 = node2;
 		}
 		
 		// Adds shared types of functions between the same pair of nodes
@@ -209,7 +340,7 @@ public class FPAnalyzer{
 				}
 			}
 			if (!node.isSelected){
-				this.weight
+				//this.weight
 			}
 			System.out.println("El nombre de quien me ha invocado es " + node.name);
 		}
@@ -233,7 +364,7 @@ public class FPAnalyzer{
 	}
 	
 	// Creates an edge between two nodes.
-	public static Edge createEdge(String name, Node node1, Node node2, int weight){
+	public static Edge createEdge(String name, Node node1, Node node2, int weight) throws Exception{
 		Edge edge = new Edge(name, node1, node2, weight);
 		node1.edges.add(edge);
 		node2.edges.add(edge);
@@ -247,17 +378,12 @@ public class FPAnalyzer{
 		return allEdges.remove(edge);
 	}
 	
-	public static int obtainSum(){
-		int sum = 0;
-		for(int i = 0; i < allNodes.size(); ++i){
-			if(allNodes.get(i).isSelected){
-				sum += allNodes.get(i).weight;
-			}
-		}
-		return sum;
+	public static void updateTotal(int value){
+		total += value;
+		System.out.println("Nuevo total: " + total);
 	}
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
 		System.out.println("Inicio del programa.");
 		// Se inician las ventanas
 		
@@ -269,18 +395,37 @@ public class FPAnalyzer{
 		// Se ejecuta el programa
 		Node rq1 = createNode("RQ 1", 20);
 		Node rq2 = createNode("RQ 2", 30);
-		Node rq3 = createNode("RQ 3", 23);
-		Node rq4 = createNode("RQ 4", 47);
+		//Node rq3 = createNode("RQ 3", 23);
+		//Node rq4 = createNode("RQ 4", 47);
 		
 		createEdge("ILF1", rq1, rq2, 7);
-		createEdge("ILF2", rq1, rq3, 4);
-		createEdge("ILF3", rq1, rq4, 5);
+		System.out.println("Fus");
+		//createEdge("ILF2", rq1, rq3, 4);
+		//createEdge("ILF3", rq1, rq4, 5);
 		
+		System.out.println("1. RQ1 = " + rq1.weight);
+		System.out.println("1. RQ2 = " + rq2.weight);
 		rq1.select();
-		//rq2.select();
-		
-		System.out.println("El valor total del programa es " + obtainSum());
-		
+		System.out.println("2. RQ1 = " + rq1.weight);
+		System.out.println("2. RQ2 = " + rq2.weight);
+		rq2.select();
+		System.out.println("3. RQ1 = " + rq1.weight);
+		System.out.println("3. RQ2 = " + rq2.weight);
+		rq1.select();
+		System.out.println("4. RQ1 = " + rq1.weight);
+		System.out.println("4. RQ2 = " + rq2.weight);
+		rq1.select();
+		System.out.println("5. RQ1 = " + rq1.weight);
+		System.out.println("5. RQ2 = " + rq2.weight);
+		updateTotal(0);
+		System.out.println("6. RQ1 = " + rq1.weight);
+		System.out.println("6. RQ2 = " + rq2.weight);
+		rq2.select();
+		System.out.println("7. RQ1 = " + rq1.weight);
+		System.out.println("7. RQ2 = " + rq2.weight);
+		rq1.select();
+		System.out.println("8. RQ1 = " + rq1.weight);
+		System.out.println("8. RQ2 = " + rq2.weight);
 		
 		
 		// Revisar si funciona bien la inicialización de un TypeOfFunction si lo creo con un List<Node>. Si no funciona correctamente será porque la lista original se borra.
